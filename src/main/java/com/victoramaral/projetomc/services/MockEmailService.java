17e3d6543bea8dc66051c7f93dd4bec1f1341c8b
Victor Amaral <tito.victor@hotmail.com>
2021-03-17 22:28:55 -0300

    SmtpService com Google

--- src/main/java/com/victoramaral/projetomc/services/MockEmailService.java
+++ src/main/java/com/victoramaral/projetomc/services/MockEmailService.java
@@ -1,9 +1,13 @@
 package com.victoramaral.projetomc.services;
 
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
+
+import com.victoramaral.projetomc.domain.Pedido;
 
 
 
@@ -16,6 +20,15 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info("Simulando envio de email...");
 		LOG.info(msg.toString());
 		LOG.info("Email Enviado");
+	}
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email Enviado");
+		
 	}
 	
 	

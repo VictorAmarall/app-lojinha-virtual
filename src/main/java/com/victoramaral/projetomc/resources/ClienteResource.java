497d9d7c417c788ff848b5fa264552a231521dbb
Victor Amaral <tito.victor@hotmail.com>
2021-03-19 11:24:14 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/victoramaral/projetomc/resources/ClienteResource.java
+++ src/main/java/com/victoramaral/projetomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +45,7 @@ public class ClienteResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-
+	
 	@RequestMapping(value = "/{id}" , method=RequestMethod.PUT)
 	public ResponseEntity <Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
 		Cliente obj = service.fromDTO(objDTO);
@@ -53,11 +54,13 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value= "/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findALL(){
 		List<Cliente> list =  service.findALL();

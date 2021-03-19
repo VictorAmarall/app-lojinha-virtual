0699c38c3896fd65a1d39dd566985f57388bf90c
Victor Amaral <tito.victor@hotmail.com>
2021-03-19 11:13:21 -0300

    Implementando autorizacao

--- src/main/java/com/victoramaral/projetomc/config/SecurityConfig.java
+++ src/main/java/com/victoramaral/projetomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.victoramaral.projetomc.security.JWTAuthenticationFilter;
+import com.victoramaral.projetomc.security.JWTAuthorizationFilter;
 import com.victoramaral.projetomc.security.JWTUtil;
 
 @Configuration
@@ -64,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-	//	http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	@Override
